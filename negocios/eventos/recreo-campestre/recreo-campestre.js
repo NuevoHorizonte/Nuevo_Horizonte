f2778e9d132a4d8d028850c51f48770ee336ede2
zzenisx00 <roggerdr1106@gmail.com>
2025-10-21 16:40:57 -0500

    recreo-campestre-v11

--- negocios/eventos/recreo-campestre/recreo-campestre.js
+++
@@ -1,9 +0,0 @@
-// --- MENÚ HAMBURGUESA ---
-document.addEventListener("DOMContentLoaded", () => {
-  const hamburger = document.getElementById("hamburger");
-  const menu = document.getElementById("menu");
-
-  hamburger.addEventListener("click", () => {
-    menu.classList.toggle("active");
-  });
-});
